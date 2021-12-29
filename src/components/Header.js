1bf6b2aed01bcc8ab805cdd8a53fff41f4b694eb
apalacios <apalacios.dev@gmail.com>
2021-12-29 15:18:10 +0100

    2112291518

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
-
+import header from '../assets/css/header'
+import {createUseStyles} from 'react-jss'
 
 const Header = ({data}) => {
-  console.log(data.name)
+  const useStyles = createUseStyles(header);
+  const classes = useStyles();
+  
+
   return (
     <div>
-      <p>{data.welcomeTitle}</p>
+      <p className={classes.letras}>{data.welcomeTitle}</p>
       <p className='name'>{data.name}</p>
       <p>{data.descriptionTitle}</p>
     </div>
