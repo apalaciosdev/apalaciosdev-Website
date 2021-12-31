0802b82c8b60b0e13c398986764f80c3034a1d88
apalacios <apalacios.dev@gmail.com>
2021-12-31 15:41:35 +0100

    2112311541

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import header from '../assets/styles/jss/header'
-import {createUseStyles} from 'react-jss'
+import React from "react";
+import header from "../assets/styles/jss/header";
+import { createUseStyles } from "react-jss";
 import { Link, animateScroll as scroll } from "react-scroll";
+import { FaAngleDoubleDown } from "react-icons/fa";
 
-
-const Header = ({data}) => {
+const Header = ({ data }) => {
   const useStyles = createUseStyles(header);
   const classes = useStyles();
-  
 
   return (
-    <div class="area" id='header'>
+    <div class="area" id="header">
       <div className={classes.generalDiv}>
         <p className={classes.letras}>{data.welcomeTitle}</p>
-        <h2 className={classes.nameTitle}>{data.name} <span className={classes.surnameTitle}>{data.surname}</span></h2>
+        <h2 className={classes.nameTitle}>
+          {data.name}{" "}
+          <span className={classes.surnameTitle}>{data.surname}</span>
+        </h2>
         <p className={classes.letras}>{data.descriptionTitle}</p>
         <ul class="circles">
           <li></li>
@@ -28,10 +30,7 @@ const Header = ({data}) => {
           <li></li>
         </ul>
 
-        
-        
-        <p className="scrolldown" className={classes.button}>
-          
+
         <Link
           activeClass="active"
           to="about"
@@ -39,11 +38,14 @@ const Header = ({data}) => {
           smooth={true}
           offset={-70}
           duration={1000}
-        >sis</Link>
-        </p>
-      </div >
+        >
+          
+        <FaAngleDoubleDown className="scroll-down"/>
+         
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
