24ee297624d0389baafe099c20e60a463e4d4b36
apalacios <apalacios.dev@gmail.com>
2022-01-01 12:50:19 +0100

    2201011250

---
+++ src/assets/styles/jss/buttonLink.js
@@ -0,0 +1,14 @@
+import {Colors} from '../colors'
+
+const ButtonLink = {
+  
+  button: {
+    borderRadius: 100,
+    fontSize: 20,
+
+    zIndex: 2
+  }
+
+}
+
+export default ButtonLink
--- src/components/ButtonLink.js
+++ src/components/ButtonLink.js
@@ -2,17 +2,25 @@ import React from "react";
 import { FaGithub,} from "react-icons/fa";
 import { BiCodeAlt } from "react-icons/bi";
 
+import buttonLink from '../assets/styles/jss/buttonLink'
+import {createUseStyles} from 'react-jss'
+
+
+
 const ButtonLink = ({ type }) => {
+  const useStyles = createUseStyles(buttonLink);
+  const css = useStyles();
+  
   const name = type;
   return (
     <div>
       {(() => {
         switch (name) {
           case "github":
-            return  <button><FaGithub/> Github</button>;
+            return  <button className={css.button}><FaGithub/> Github</button>;
 
           case "code":
-            return <button><BiCodeAlt/> Projects</button>;
+            return <button className={css.button}><BiCodeAlt/> Projects</button>;
 
           default:
             return <div>You are a User.</div>;
