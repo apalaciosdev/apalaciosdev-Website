53470b1ab1ff5262facf94347ecd2a335c637e32
apalacios <apalacios.dev@gmail.com>
2022-01-01 12:11:14 +0100

    2201011211

--- src/App.js
+++ src/App.js
@@ -13,32 +13,12 @@ function App() {
     AOS.init();
     AOS.refresh();
   }, []);
-  
+
   return (
     <div className="App">
       <Navbar/>
       <Header data={data.main}/>
       <About/>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, soluta maxime accusamus ducimus recusandae, magnam aut exercitationem fugiat reprehenderit animi tempora nulla rem fugit quaerat optio eveniet dolorum eum. Quaerat?
-      Ipsa repellendus asperiores vel consequatur, aut ad consectetur repudiandae sed labore dolorem voluptatem quisquam similique, eligendi possimus sint. Quibusdam blanditiis accusamus eveniet autem ad excepturi fugit quas esse cum incidunt.
-      Reprehenderit id corrupti, libero accusantium, rem illum labore repudiandae ut ab fuga omnis! Quidem totam qui ex error alias, iste iusto. Aperiam sed ex ducimus quae cum facilis commodi delectus.
-      Quaerat maiores deserunt sunt excepturi, quia et assumenda ad officiis! Natus, ullam laboriosam facilis officia sed ipsam voluptatem quam? Perferendis facilis laboriosam sequi maiores harum accusamus adipisci, labore vitae aliquid!
-      Impedit consequuntur, eos perspiciatis voluptates doloremque dolore nostrum officiis eius provident quae, aliquam rem? Perferendis ipsum adipisci a sequi corrupti. Repellat libero soluta molestiae? Dolores velit itaque debitis nostrum consectetur.
-      Nesciunt magnam labore consectetur sunt nostrum ipsa minima deserunt et ullam sed praesentium excepturi, illum eligendi. Autem perferendis similique, consequatur iste quidem eaque, mollitia reiciendis molestiae eos fugit illo ratione?
-      Tempora quaerat sint aspernatur sit at placeat necessitatibus magnam autem animi! Nihil iusto facere voluptatum accusamus libero quas amet voluptatibus neque expedita, cumque veniam aliquid. Repudiandae beatae magni quis voluptatibus!
-      Fuga dolor sit reprehenderit porro veritatis cupiditate voluptate maxime, laborum quis commodi placeat totam eveniet quaerat aliquid nobis cum debitis facilis ipsa sed quae tempora dolore aliquam mollitia. Incidunt, architecto?
-      Quis, ipsa nam necessitatibus totam esse, ex sed, alias quibusdam quaerat earum culpa distinctio inventore deleniti doloribus numquam quasi? In, modi? Similique quam ad quo doloremque, fugit nesciunt impedit dolorem.
-      In nostrum facere unde sapiente asperiores voluptate nesciunt culpa ut sit qui excepturi eaque pariatur magnam, eos beatae optio praesentium deserunt esse velit expedita dicta explicabo iure debitis aperiam. Fuga!
-      Provident sed ratione corrupti culpa reprehenderit assumenda qui voluptatum eveniet harum ullam voluptas, vitae suscipit illo cum, laudantium temporibus quo repellendus ipsum velit sit maiores quidem eum. A, omnis impedit?
-      Fugit earum omnis libero illo iure quod amet beatae nostrum labore sunt? Labore dolore fugit asperiores? Minus quia illum nostrum, et error recusandae dignissimos ipsam iure vitae officiis mollitia voluptate.
-      Nam saepe ratione repellendus perferendis explicabo quae, ab sapiente repudiandae quidem neque aut, autem temporibus harum id qui laborum quibusdam vero totam necessitatibus nisi! Quasi delectus odio qui omnis optio.
-      Veniam, possimus. Temporibus eos harum nam esse dolorum dolorem? Ad corrupti atque asperiores sunt natus deleniti excepturi, eos aliquid voluptate esse sequi nostrum modi eaque totam adipisci tenetur, quia officiis!
-      Voluptas, eveniet incidunt ratione cupiditate deleniti atque soluta explicabo beatae animi. Tempore, animi? Possimus incidunt molestias voluptatibus nostrum modi neque expedita quis itaque. Libero explicabo inventore magnam, officia expedita neque.
-      Consequatur molestias a enim? Magni asperiores unde fugit! Inventore, expedita consectetur, suscipit nobis hic et totam reprehenderit rerum impedit tenetur quae asperiores numquam recusandae a dignissimos eos repellat. Quibusdam, iste.
-      Esse dolores fuga voluptatum inventore aperiam in harum ea architecto minus nihil hic nisi dignissimos repudiandae amet temporibus distinctio reiciendis, voluptatem corporis eius nemo. Necessitatibus similique nihil minus quaerat itaque?
-      Quod, eaque accusamus soluta magni eum ipsa cupiditate obcaecati temporibus aperiam natus itaque, veritatis inventore quos dolor aspernatur facilis voluptatibus eos, dolores voluptatum fugiat velit? Placeat odio quidem rerum id?
-      Autem velit sit voluptate nemo id, error saepe cum laboriosam amet officia fugit iure unde, quo voluptatibus porro odio? Possimus iste doloremque aliquam, dolores quidem nisi exercitationem praesentium autem adipisci.
-      Eligendi asperiores iste molestias, nobis tempore neque quo, quasi voluptatum quod sint rem error quisquam, sed excepturi molestiae? Minima magnam tenetur rerum, eveniet suscipit molestias laudantium nobis placeat mollitia impedit.</p>
     </div>
   );
 }
