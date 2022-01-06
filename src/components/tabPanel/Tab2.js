f0ba2afe555dd6736ca0428af512d7af9b8f7d3b
apalacios <apalacios.dev@gmail.com>
2022-01-06 13:54:47 +0100

    2201061220

--- src/components/tabPanel/Tab2.js
+++ src/components/tabPanel/Tab2.js
@@ -10,10 +10,10 @@ const Tab2 = () => {
         <li><Icon iconType={'nextjs'} bg={'white'} size={'30'}/><p>NextJS</p></li>
         <li><Icon iconType={'nodejs'} bg={'nodeJS'} size={'30'}/><p>NodeJS</p></li>
         <li><Icon iconType={'express'} bg={'express'} size={'30'}/><p>Express</p></li>
+        <li><Icon iconType={'mongodb'} bg={'mongoDB'} size={'30'}/><p>MongoDB</p></li>
         <li><Icon iconType={'js'} bg={'js'} size={'30'}/><p>JS</p></li>
         <li><Icon iconType={'html'} bg={'html'} size={'30'}/><p>HTML</p></li>
         <li><Icon iconType={'css'} bg={'css'} size={'30'}/><p>CSS</p></li>
-        <li><Icon iconType={'mongodb'} bg={'mongoDB'} size={'30'}/><p>MongoDB</p></li>
         <li><Icon iconType={'styledcomponents'} bg={'styledComponents'} size={'30'}/><p>Styled Components</p></li>
         <li><Icon iconType={'jss'} bg={'js'} size={'30'}/><p>JSS</p></li>
         <li><Icon iconType={'photoshop'} bg={'photoshop'} size={'30'}/><p>Photoshop</p></li>
