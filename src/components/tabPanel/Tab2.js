2ba273a3b6442a4a2d71c3a8afcd329e1562ebb2
apalacios <apalacios.dev@gmail.com>
2022-01-06 10:27:05 +0100

    2201061027

--- src/components/tabPanel/Tab2.js
+++ src/components/tabPanel/Tab2.js
@@ -3,7 +3,11 @@ import React from 'react'
 const Tab2 = () => {
   return (
     <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur veniam nisi reprehenderit ullam veritatis eaque explicabo unde voluptatem earum eligendi. Ipsum reiciendis voluptatum inventore aspernatur, quasi praesentium vero omnis voluptatibus! Nobis aperiam sed incidunt accusamus neque, debitis hic id obcaecati similique ullam reiciendis officia commodi sapiente aliquid quos, a laborum inventore, consectetur at ab. Ex odit impedit quam eligendi delectus?</p>
+      <ul>
+        <li>React</li>
+        <li>HTML</li>
+        <li>CSS</li>
+      </ul>
     </div>
   )
 }
