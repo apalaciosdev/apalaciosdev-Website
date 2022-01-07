72d575c5d06d68b212f820a9e853904087496da8
apalacios <apalacios.dev@gmail.com>
2022-01-07 12:44:05 +0100

    2201071244

---
+++ src/assets/styles/jss/card.js
@@ -0,0 +1,22 @@
+import {Colors} from '../colors'
+
+
+const card = {
+  generalDivCard: {
+    display: 'flex',
+    justifyContent: 'center',
+
+  },
+  
+  divCard: {
+    display: 'grid',
+    justifyItems: 'center',
+    border: '1px solid black',
+  },
+
+  divProjects: {
+    
+  }
+}
+
+export default card;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
+import projects from "../assets/styles/jss/projects";
+import { createUseStyles } from "react-jss";
+import card from '../assets/styles/jss/card';
+
 const Card = ({title, description, img}) => {
-  console.log(`../assets/images/${img}`)
+  const useStyles = createUseStyles(card);
+  const css = useStyles();
+
   return (
-    <div>
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <img src={`/images/${img}`} alt="" />
+    <div className={css.generalDivCard}>
+      <div className={css.divCard}>
+        <h3 h3>{title}</h3>
+        <p>{description}</p>
+        <img src={`/images/${img}`} alt="" />
+      </div>
     </div>
   )
 }
