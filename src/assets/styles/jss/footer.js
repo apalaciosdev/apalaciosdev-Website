d96daa2b978c12fca6b518f0266085cf1ee23e24
apalacios <apalacios.dev@gmail.com>
2022-01-09 14:22:02 +0100

    2201091421

--- src/assets/styles/jss/footer.js
+++ src/assets/styles/jss/footer.js
@@ -2,15 +2,26 @@ import {Colors} from '../colors'
 
 
 const footer = {
+  generalDivFooter: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    background: Colors.deadGrey,
+    color: 'white',
+    height: 120,
+    width: '100%',
+    marginTop: 100,
+    paddingTop: 30,
+    paddingBottom: 25
+  },
+  
   divFooter: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    background: Colors.deadGrey,
-    color: 'white',
-    height: 75,
     width: '100%',
-    marginTop: 100,
   },
   
   
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,16 +8,18 @@ const Footer = () => {
   const useStyles = createUseStyles(footer);
   const css = useStyles();
   return (
-    <footer className={css.divFooter}>
-      <button onClick={() => (window.open('ff'))}>
-        <Icon iconType={'github'} size={'30'} bgHover={'grey'}/>
-      </button>
-      <button onClick={() => (window.open('ff'))}>
-        <Icon iconType={'linkedin'} size={'30'} bgHover={'grey'}/>
-      </button>
-      <button onClick={() => (window.open('ff'))}>
-        <Icon iconType={'mail'} size={'30'} bgHover={'grey'}/>
-      </button>
+    <footer className={css.generalDivFooter}>
+      <div className={css.divFooter}>
+        <button onClick={() => (window.open('ff'))}>
+          <Icon iconType={'github'} size={'30'} bgHover={'grey'}/>
+        </button>
+        <button onClick={() => (window.open('ff'))}>
+          <Icon iconType={'linkedin'} size={'30'} bgHover={'grey'}/>
+        </button>
+        <button onClick={() => (window.open('ff'))}>
+          <Icon iconType={'mail'} size={'30'} bgHover={'grey'}/>
+        </button>
+      </div>
       <p>Developed in React by apalacios.dev</p>
     </footer>
   )
