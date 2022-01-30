39bcd767291ddc1068ff01143d292103a87fcea0
apalacios <apalacios.dev@gmail.com>
2022-01-30 15:42:44 +0100

    2201301542

--- src/components/tabPanel/Tab1.js
+++ src/components/tabPanel/Tab1.js
@@ -9,7 +9,9 @@ const Tab1 = () => {
         I am a JR Developer motivated to learn new technologies, especially in
         JavaScript ecosystem. <br />
 
+        Desde pequeño se me inculcó el siguiente dicho:
         My mantra: trabaja duro y lo consigues. Por esta razon, intento invertir mi tiempo en autoformarme y crear proyectos que puedan cubrir necesidades.
+
 
       </p>
       <Button2 type={"cv"} colorType={"neon"} text={"Download CV"}></Button2>
