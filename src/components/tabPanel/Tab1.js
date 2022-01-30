067d047b5f039b68f1a7736ef73f2afdc8687e5f
apalacios <apalacios.dev@gmail.com>
2022-01-30 13:22:00 +0100

    2201301321

--- src/components/tabPanel/Tab1.js
+++ src/components/tabPanel/Tab1.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from "react"
 
-import Button2 from '../buttons/Button2'
+import Button2 from "../buttons/Button2"
 
 const Tab1 = () => {
   return (
     <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur veniam nisi reprehenderit ullam veritatis eaque explicabo unde voluptatem earum eligendi. Ipsum reiciendis voluptatum inventore aspernatur, quasi praesentium vero omnis voluptatibus!
-      Nobis aperiam sed incidunt accusamus neque, debitis hic id obcaecati similique ullam reiciendis officia commodi sapiente aliquid quos, a laborum inventore, consectetur at ab. Ex odit impedit quam eligendi delectus?</p>
-      <Button2 type={'cv'} colorType={'neon'} text={'Download CV'}></Button2>
-      
-    </div>
+      <p>
+        I am a JR Developer motivated to learn new technologies, especially in
+        JavaScript ecosystem. <br />
 
+        My mantra: trabaja duro y lo consigues. Por esta razon, intento invertir mi tiempo en autoformarme y crear proyectos que puedan cubrir necesidades.
+
+      </p>
+      <Button2 type={"cv"} colorType={"neon"} text={"Download CV"}></Button2>
+    </div>
   )
 }
 
