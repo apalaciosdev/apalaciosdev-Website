ccbf4293c7b88e9079e295856e45bf66808c0851
apalacios <apalacios.dev@gmail.com>
2022-01-30 16:19:52 +0100

    2201301619

--- src/components/tabPanel/Tab1.js
+++ src/components/tabPanel/Tab1.js
@@ -4,15 +4,18 @@ import Button2 from "../buttons/Button2"
 
 const Tab1 = () => {
   return (
-    <div>
+    <div className="tab1">
       <p>
         I am a JR Developer motivated to learn new technologies, especially in
-        JavaScript ecosystem. <br />
-
-        Desde pequeño se me inculcó el siguiente dicho:
-        My mantra: trabaja duro y lo consigues. Por esta razon, intento invertir mi tiempo en autoformarme y crear proyectos que puedan cubrir necesidades.
-
-
+        JavaScript ecosystem.
+      </p>
+      <p>
+        My mantra: <cite>`Nothing is impossible, but it only comes with hard work.´</cite> <br/>For
+        this reason, I try to spend my time in learn and create projects
+        that can help people.
+      </p>
+      <p>
+        Actually, I develop projects in MERN Stack (mongoDB, expressJS, reactJS y nodeJS).
       </p>
       <Button2 type={"cv"} colorType={"neon"} text={"Download CV"}></Button2>
     </div>
