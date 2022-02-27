3baede0c058e4e945a39a21cbe86d1063edf4ec7
Aaron Palacios Olea <74265184+apalaciosdev@users.noreply.github.com>
2022-02-27 11:44:48 +0100

    Update Tab1.js

--- src/components/tabPanel/Tab1.js
+++ src/components/tabPanel/Tab1.js
@@ -15,7 +15,7 @@ const Tab1 = () => {
         that can help people.
       </p>
       <p>
-        Actually, I develop projects in MERN Stack (mongoDB, expressJS, reactJS y nodeJS).
+        Actually, I develop projects in MERN Stack (mongoDB, expressJS, reactJS & nodeJS).
       </p>
       <Button2 type={"cv"} colorType={"neon"} text={"Download CV"}></Button2>
     </div>
