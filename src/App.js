29554dc07987f63910804ef90bf82b125009d446
Aaron̨̙̹̦̱͗̈̋̐̈́͝ Palacios͍͇̄̑͂̾̏̐ <74265184+apalaciosdev@users.noreply.github.com>
2024-02-14 16:50:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import React, {useEffect} from 'react'
 
-import {data} from './resumeData.js'
-
-import AOS from "aos";
-import "aos/dist/aos.css";
-
-
 function App() {
-  useEffect(() => {
-    AOS.init();
-    AOS.refresh();
-  }, []);
-
+ 
   return (
     <div></div>
   );
